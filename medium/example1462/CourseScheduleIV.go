d0a1f55d37e79ee5bcca0bb594d472ef945708ee
J1nAB1n9 <J1nAB1n9@outlook.com>
2023-09-12 21:16:46 +0800

    打卡

--- medium/example1462/CourseScheduleIV.go
+++ medium/example1462/CourseScheduleIV.go
@@ -1,11 +1,9 @@
 package example1462
 
-import "container/list"
-
 func checkIfPrerequisite(numCourses int, prerequisites [][]int, queries [][]int) []bool {
 	queriesSize := len(queries)
 	answer := make([]bool, queriesSize)
-
+	suffix := make(map[int][]int)
 	var pd [][]bool
 	for i := 0; i < numCourses; i++ {
 		pd = append(pd, make([]bool, numCourses))
@@ -13,18 +11,23 @@ func checkIfPrerequisite(numCourses int, prerequisites [][]int, queries [][]int)
 
 	for _, v := range prerequisites {
 		pd[v[0]][v[1]] = true
+		suffix[v[0]] = append(suffix[v[0]], v[1])
+	}
+
+	for i := 0; i < numCourses; i++ {
+		for j := 0; j < len(suffix[i]); j++ {
+			v := suffix[i][j]
+			for k := 0; k < numCourses; k++ {
+				if pd[v][k] && !pd[i][k] {
+					pd[i][k] = true
+					suffix[i] = append(suffix[i], k)
+				}
+			}
+		}
 	}
 
 	for i, q := range queries {
-		_q0 := q[0]
-		_q1 := q[1]
-
-		if pd[_q0][_q1] {
-			answer[i] = true
-			continue
-		}
-
-		list.New()
+		answer[i] = pd[q[0]][q[1]]
 	}
 
 	return answer
