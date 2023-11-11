91613d210829033d1a15ae2517676ee1137c8200
J1nAB1n9 <J1nAB1n9@outlook.com>
2023-11-11 19:07:35 +0800

    is work

--- medium/example2300/leetcode_test.go
+++ medium/example2300/leetcode_test.go
@@ -5,14 +5,23 @@ import (
 	"testing"
 )
 
-func TestExample1(t *testing.T) {
-	fmt.Println(successfulPairs([]int{1, 2, 3}, []int{1, 2, 6, 7, 9}, 5))
-}
-
-func TestExample2(t *testing.T) {
-	fmt.Println(successfulPairs([]int{5, 1, 3}, []int{1, 2, 3, 4, 5}, 7))
-}
-
-func TestExample3(t *testing.T) {
-	fmt.Println(successfulPairs([]int{3, 1, 2}, []int{8, 5, 8}, 16))
+//
+//func TestExample1(t *testing.T) {
+//	fmt.Println(successfulPairs([]int{1, 2, 3}, []int{1, 2, 6, 7, 9}, 5))
+//}
+//
+//func TestExample2(t *testing.T) {
+//	fmt.Println(successfulPairs([]int{5, 1, 3}, []int{1, 2, 3, 4, 5}, 7))
+//}
+//
+//func TestExample3(t *testing.T) {
+//	fmt.Println(successfulPairs([]int{3, 1, 2}, []int{8, 5, 8}, 16))
+//}
+//
+//func TestExample4(t *testing.T) {
+//	fmt.Println(successfulPairs([]int{15,8,19} ,[]int{38,36,23},328))
+//}
+//
+func TestExample5(t *testing.T) {
+	fmt.Println(successfulPairs([]int{14}, []int{25, 19, 30, 37, 14, 30, 38, 22, 38, 38, 26, 33, 34, 23, 40, 28, 15, 29, 36, 39, 39, 37, 32, 38, 8, 17, 39, 20, 4, 39, 39, 7, 30, 35, 29, 23}, 317))
 }
--- medium/example2300/successfulPairs.go
+++ medium/example2300/successfulPairs.go
@@ -1,11 +1,64 @@
 package example2300
 
-import (
-	"sort"
-)
+import "sort"
+
+//func successfulPairs(spells []int, potions []int, success int64) []int {
+//	sort.Ints(potions)
+//	ans := make([]int, len(spells))
+//	for i := 0; i < len(spells); i++ {
+//		minPotions := int(success / int64(spells[i]))
+//		if int64(spells[i]) > success {
+//			minPotions = 0 // 处理特殊情况
+//		} else if int64(minPotions)*int64(spells[i]) != success {
+//			minPotions += 1
+//		}
+//
+//		l, r := 0, len(potions)-1
+//
+//		index := -1
+//		// mid select
+//		for l <= r {
+//			mid := (l + r) / 2
+//			if potions[mid] == minPotions {
+//				index = mid
+//				break
+//			} else if potions[mid] > minPotions {
+//				r = mid - 1
+//			} else {
+//				l = mid + 1
+//			}
+//		}
+//
+//		if index != -1 {
+//			ans[i] = len(potions) - index
+//		} else {
+//			if l >= len(potions) {
+//				ans[i] = 0
+//			} else if r==-1||l==-1{
+//				ans[i] = len(potions)
+//			}else if potions[r] > minPotions {
+//				ans[i] = len(potions) - r
+//			} else if potions[l] > minPotions {
+//				ans[i] = len(potions) - l
+//			} else {
+//				ans[i] = 0
+//			}
+//		}
+//
+
+//	}
+//	return ans
+//}
 
 func successfulPairs(spells []int, potions []int, success int64) []int {
 	sort.Ints(potions)
+	suffix := make(map[int]int)
+	for i, v := range potions {
+		if _, has := suffix[v]; has == false {
+			suffix[v] = i
+		}
+	}
+
 	ans := make([]int, len(spells))
 	for i := 0; i < len(spells); i++ {
 		minPotions := int(success / int64(spells[i]))
@@ -32,14 +85,19 @@ func successfulPairs(spells []int, potions []int, success int64) []int {
 		}
 
 		if index != -1 {
+			index := suffix[potions[index]]
 			ans[i] = len(potions) - index
 		} else {
 			if l >= len(potions) {
 				ans[i] = 0
+			} else if r == -1 || l == -1 {
+				ans[i] = len(potions)
 			} else if potions[r] > minPotions {
-				ans[i] = len(potions) - r
+				index := suffix[potions[r]]
+				ans[i] = len(potions) - index
 			} else if potions[l] > minPotions {
-				ans[i] = len(potions) - l
+				index := suffix[potions[l]]
+				ans[i] = len(potions) - index
 			} else {
 				ans[i] = 0
 			}
